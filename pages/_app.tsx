2ddd644065adb6f3584be4fb9344b29ea9c6ea21
lmachens <leon@machens.koeln>
2021-03-25 15:24:09 +0100

    ♻️ Add Cart as children

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import AppHeader from "../components/AppHeader";
+import Cart from "../components/Cart";
 import "../styles/globals.css";
 import { Item } from "../utils/types";
 
@@ -9,7 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <AppHeader items={items} onItemsClear={() => setItems([])} />
+      <AppHeader>
+        <Cart items={items} onClear={() => setItems([])} />
+      </AppHeader>
 
       <Component
         onItemAdd={(item: Item) => setItems((items) => [...items, item])}
