cf49713a062a3458e0ca613405c46a589a4baa26
DNaunin <d.naunin@gmail.com>
2021-04-12 16:28:46 +0200

    ✨ First steps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,33 @@
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import AppHeader from "../components/AppHeader";
+import Cart from "../components/Cart";
+import { CartContext } from "../contexts/CartContext";
 import "../styles/globals.css";
 import { Item } from "../utils/types";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [items, setItems] = useState<Item[]>([]);
 
+  function onClear() {
+    setItems([]);
+  }
   return (
-    <>
-      <AppHeader items={items} onItemsClear={() => setItems([])} />
+    <CartContext.Provider
+      value={{
+        items,
+        onClear,
+      }}
+    >
+      <AppHeader>
+        <Cart />
+      </AppHeader>
 
       <Component
         onItemAdd={(item: Item) => setItems((items) => [...items, item])}
         {...pageProps}
       />
-    </>
+    </CartContext.Provider>
   );
 }
 
